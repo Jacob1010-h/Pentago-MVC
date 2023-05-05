21b12506cdd5bd5f8def95411f5f06d81fa39305
Jacob Hotz <lordgaming1010@gmail.com>
2023-05-05 14:22:18 -0700

    move miniBoardIndex to inside the for loop :DD

--- src/game/Board.java
+++ src/game/Board.java
@@ -31,10 +31,11 @@ public class Board {
     public void populateBoard(MiniBoardHelper miniBoardHelper) {
         MiniBoard[] miniBoards = miniBoardHelper.getMiniBoards();
 
-        // combine all miniBoards into one big board (board)
-        int miniBoardIndex = 0;
+        // combine all miniBoards into one big board
+        // loop through the board, incrementing by the size of a miniBoard (3)
+        // for each cell in board, set the value to the value of the corresponding cell in the miniBoard
         for (int i = 0; i < Constants.BOARD_SIZE; i += Constants.MINI_BOARD_SIZE) {
-            for (int j = 0; j < Constants.BOARD_SIZE; j += Constants.MINI_BOARD_SIZE) {
+            for (int j = 0, miniBoardIndex = 0; j < Constants.BOARD_SIZE; j += Constants.MINI_BOARD_SIZE) {
                 for (int k = 0; k < Constants.MINI_BOARD_SIZE; k++) {
                     for (int l = 0; l < Constants.MINI_BOARD_SIZE; l++) {
                         this.board[i + k][j + l].setValue(miniBoards[miniBoardIndex].getCell(k, l).getValue());
