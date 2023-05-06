93da5c20cc1c7d2026e190f93cfb93b89bc8a93c
Jacob1010-h <lordgaming1010@gmail.com>
2023-05-05 22:56:47 -0700

    Add Java Docs Replace "this.board =" with "this.setCell()" Replace ".getCell(k, l).getValue() with ".getCellValue(k, l)"

--- src/game/Board.java
+++ src/game/Board.java
@@ -20,25 +20,47 @@ public class Board {
         }
     }
 
+    /**
+     * The getCell function returns the BoardCell object at a given row and column.
+     *
+     * @param row Specify the row of the cell that is being returned
+     * @param col Specify the column of the cell that is being returned
+     */
     public BoardCell getCell(int row, int col) {
         return this.board[row][col];
     }
 
+    /**
+     * The setCell function sets the value of a cell in the board.
+     *
+     * @param row the row of the cell
+     * @param col the column of the cell
+     * @param value the value of a cell
+     *
+     */
     public void setCell(int row, int col, int value) {
         this.board[row][col].setValue(value);
     }
 
-    public void populateBoard(MiniBoardHelper miniBoardHelper) {
+
+    // create a copy board method that copies a mini board to a quadrant of the board
+
+    /**
+     * The copyBoard function takes a MiniBoard[] object as arguments.
+     * The function iterates through each mini-board in the array, and then iterates through each cell in that mini-board.
+     * It then sets the value of that cell on this board to be equal to the value of that same cell on the mini-boards.
+     *
+     * @param miniBoardHelper - the MiniBoardHelper object that contains the MiniBoard[] object that we want to copy to this board.
+     *
+     */
+    public void copyBoard(MiniBoardHelper miniBoardHelper) {
         MiniBoard[] miniBoards = miniBoardHelper.getMiniBoards();
 
-        // combine all miniBoards into one big board
-        // loop through the board, incrementing by the size of a miniBoard (3)
-        // for each cell in board, set the value to the value of the corresponding cell in the miniBoard
         for (int i = 0; i < Constants.BOARD_SIZE; i += Constants.MINI_BOARD_SIZE) {
             for (int j = 0, miniBoardIndex = 0; j < Constants.BOARD_SIZE; j += Constants.MINI_BOARD_SIZE) {
                 for (int k = 0; k < Constants.MINI_BOARD_SIZE; k++) {
                     for (int l = 0; l < Constants.MINI_BOARD_SIZE; l++) {
-                        this.board[i + k][j + l].setValue(miniBoards[miniBoardIndex].getCell(k, l).getValue());
+                        this.setCell(i + k, j + l, miniBoards[miniBoardIndex].getCellValue(k, l));
                     }
                 }
                 miniBoardIndex++;
--- src/game/MiniBoard.java
+++ src/game/MiniBoard.java
@@ -1,70 +1,122 @@
 package game;
 
-public class MiniBoard {
-	private BoardCell[][] miniBoard;
+public class MiniBoard extends BoardCell {
+    private BoardCell[][] miniBoard;
 
-	public MiniBoard(BoardCell[][] miniBoard){
-		this.miniBoard = miniBoard;
+    public MiniBoard(BoardCell[][] miniBoard) {
+        this.miniBoard = miniBoard;
 //		initMiniBoard();
-		initMiniBoardRand();
-	}
+        initMiniBoardRand();
+    }
+
+    public MiniBoard() {
+    }
 
 
-	public BoardCell[][] getMiniBoard(){
-		return miniBoard;
-	}
+    /**
+     * The getMiniBoard function returns the miniBoard array.
+     *
+     * @return BoardCell[][] - the miniBoard array
+     */
+    public BoardCell[][] getMiniBoard() {
+        return miniBoard;
+    }
 
-	public void initMiniBoard(){
-		for(int i = 0; i < Constants.MINI_BOARD_SIZE; i++){
-			for(int j = 0; j < Constants.MINI_BOARD_SIZE; j++){
-				this.miniBoard[i][j] = new BoardCell(Constants.EMPTY);
-			}
-		}
-	}
+    /**
+     * The initMiniBoard function initializes the miniBoard array with empty BoardCell objects.
+     */
+    public void initMiniBoard() {
+        for (int i = 0; i < Constants.MINI_BOARD_SIZE; i++) {
+            for (int j = 0; j < Constants.MINI_BOARD_SIZE; j++) {
+                this.miniBoard[i][j] = new BoardCell(Constants.EMPTY);
+            }
+        }
+    }
 
-	public void initMiniBoardRand(){
-		for(int i = 0; i < Constants.MINI_BOARD_SIZE; i++){
-			for(int j = 0; j < Constants.MINI_BOARD_SIZE; j++){
-				this.miniBoard[i][j] = new BoardCell((int) (Math.random() * 3 - 1));
-			}
-		}
-	}
+    /**
+     * The initMiniBoardRand function initializes the miniBoard array with random values.
+     */
+    public void initMiniBoardRand() {
+        for (int i = 0; i < Constants.MINI_BOARD_SIZE; i++) {
+            for (int j = 0; j < Constants.MINI_BOARD_SIZE; j++) {
+                this.miniBoard[i][j] = new BoardCell((int) (Math.random() * 3 - 1));
+            }
+        }
+    }
 
-	public BoardCell getCell(int row, int col) {
-		return this.miniBoard[row][col];
-	}
+    /**
+     * The getCell function returns the BoardCell object at a given row and column.
+     *
+     * @param row Specify the row of the cell to be returned
+     * @param col Specify the column of the cell to be returned
+     *
+     */
+    public BoardCell getCell(int row, int col) {
+        return this.miniBoard[row][col];
+    }
 
-	public void rotateClockwise() {
-		// create temp board
-		BoardCell[][] temp = new BoardCell[Constants.MINI_BOARD_SIZE][Constants.MINI_BOARD_SIZE];
-		// copy the miniBoard to the temp board
-		for (int i = 0; i < Constants.MINI_BOARD_SIZE; i++) {
-			System.arraycopy(this.miniBoard[i], 0, temp[i], 0, Constants.MINI_BOARD_SIZE);
-		}
-		// Rotate the values and apply them to miniBoard
-		for (int row = 0; row < Constants.MINI_BOARD_SIZE; row++) {
-			for (int col = 0; col < Constants.MINI_BOARD_SIZE; col++) {
-				this.miniBoard[row][col] = temp[Constants.MINI_BOARD_SIZE - col - 1][row];
-			}
-		}
+    /**
+     * The setCell function sets the cell at a given row and column to a new value.
+     *
+     * @param row Determine the row of the cell that is being set
+     * @param col Set the column of the cell
+     * @param cell Set the cell at the specified row and column
+     *
+     */
+    public void setCell(int row, int col, BoardCell cell) {
+        this.miniBoard[row][col] = cell;
+    }
+
+    /**
+     * The getCellValue function returns the value of a cell in the miniBoard.
+     *
+     * @param row Specify which row the cell is in
+     * @param col Get the column of the cell
+     * @return The value of the cell at the given row and column
+     */
+    public int getCellValue(int row, int col) {
+        return this.miniBoard[row][col].getValue();
+    }
+
+    /**
+     * The rotateClockwise function rotates the miniBoard clockwise by 90 degrees.
+     * It does this by creating a temporary board, copying the values of miniBoard to it, and then rotating those values and applying them to miniBoard.
+     */
+    public void rotateClockwise() {
+        // create temp board
+        BoardCell[][] temp = new BoardCell[Constants.MINI_BOARD_SIZE][Constants.MINI_BOARD_SIZE];
+        // copy the miniBoard to the temp board
+        for (int i = 0; i < Constants.MINI_BOARD_SIZE; i++) {
+            System.arraycopy(this.miniBoard[i], 0, temp[i], 0, Constants.MINI_BOARD_SIZE);
+        }
+        // Rotate the values and apply them to miniBoard
+        for (int row = 0; row < Constants.MINI_BOARD_SIZE; row++) {
+            for (int col = 0; col < Constants.MINI_BOARD_SIZE; col++) {
+                this.miniBoard[row][col] = temp[Constants.MINI_BOARD_SIZE - col - 1][row];
+            }
+        }
 
 
-	}
+    }
 
-	public void rotateCounterClockwise() {
-		// create temp board
-		BoardCell[][] temp = new BoardCell[Constants.MINI_BOARD_SIZE][Constants.MINI_BOARD_SIZE];
-		// copy the miniBoard to the temp board
-		for (int i = 0; i < Constants.MINI_BOARD_SIZE; i++) {
-			System.arraycopy(this.miniBoard[i], 0, temp[i], 0, Constants.MINI_BOARD_SIZE);
-		}
-		// Rotate the values and apply them to miniBoard
-		for (int row = 0; row < Constants.MINI_BOARD_SIZE; row++) {
-			for (int col = 0; col < Constants.MINI_BOARD_SIZE; col++) {
-				this.miniBoard[row][col] = temp[col][Constants.MINI_BOARD_SIZE - row - 1];
-			}
-		}
-	}
+    /**
+     * The rotateCounterClockwise function rotates the miniBoard counter-clockwise.
+     * It does this by creating a temporary board, copying the values of miniBoard to it, and then rotating those values and applying them to miniBoard.
+     */
+    public void rotateCounterClockwise() {
+        // create temp board
+        BoardCell[][] temp = new BoardCell[Constants.MINI_BOARD_SIZE][Constants.MINI_BOARD_SIZE];
+        // copy the miniBoard to the temp board
+        for (int i = 0; i < Constants.MINI_BOARD_SIZE; i++) {
+            System.arraycopy(this.miniBoard[i], 0, temp[i], 0, Constants.MINI_BOARD_SIZE);
+        }
+        // Rotate the values and apply them to miniBoard
+        for (int row = 0; row < Constants.MINI_BOARD_SIZE; row++) {
+            for (int col = 0; col < Constants.MINI_BOARD_SIZE; col++) {
+                this.miniBoard[row][col] = temp[col][Constants.MINI_BOARD_SIZE - row - 1];
+            }
+        }
+    }
 
 
 }
