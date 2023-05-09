9562666e0727f292e7a8853e0f5b8c21f1075cb4
Jacob Hotz <lordgaming1010@gmail.com>
2023-05-09 10:25:29 -0700

    create JFrame Window

--- src/game/Controller.java
+++ src/game/Controller.java
@@ -1,6 +1,8 @@
 package game;
 import com.mrjaffesclass.apcs.messenger.*;
 
+import javax.swing.*;
+
 /**
  *
  * The Controller is the master of the App you're writing. It instantiates the
@@ -12,7 +14,22 @@ import com.mrjaffesclass.apcs.messenger.*;
  * @author Roger Jaffe
  * @version 1.0
  */
-public class Controller implements MessageHandler {
+public class Controller implements MessageHandler{
+
+    Messenger mvcMessaging;
+    public Controller() {
+        this.mvcMessaging = new Messenger();
+
+        GamePanel gp = new GamePanel(mvcMessaging);
+
+        Model model = new Model(mvcMessaging);
+    }
+
+    public void init() {
+        // subscribe to messages from the view (UI)
+//        mvcMessaging.subscribe("", this);
+    }
+
 
     /**
      * Program entry -- main is called when the program starts
@@ -21,11 +38,16 @@ public class Controller implements MessageHandler {
      */
     public static void main(String[] args) {
         Controller app = new Controller();  // Create our controller...
-
+        app.init();
     }
 
     @Override
-    public void messageHandler(String s, Object o) {
-
+    public void messageHandler(String messageString, Object messageObject) {
+        if (messageObject != null) {
+            System.out.println("MSG: received by controller: "+messageString+" | "+messageObject.toString());
+        } else {
+            System.out.println("MSG: received by controller: "+messageString+" | No data sent");
+        }
+        // TODO: Add message handling code for messages received from the GamePanel
     }
 }
--- src/game/GamePanel.java
+++ src/game/GamePanel.java
@@ -1,11 +1,24 @@
 package game;
 
-import com.mrjaffesclass.apcs.messenger.MessageHandler;
-import com.mrjaffesclass.apcs.messenger.Messenger;
+import com.mrjaffesclass.apcs.messenger.*;
 
 import javax.swing.*;
 
-public class GamePanel extends JPanel implements MessageHandler {
+public class GamePanel extends JFrame implements MessageHandler {
+
+    private final Messenger mvcMessaging;
+    public GamePanel(Messenger mvcMessaging) {
+        this.mvcMessaging = mvcMessaging;
+        this.init();
+    }
+
+    public void init() {
+        this.setTitle("Tic Tac Toe");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(800, 600);
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+    }
 
     @Override
     public void messageHandler(String s, Object o) {
