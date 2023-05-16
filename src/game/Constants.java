97ef0fbdcb962edebcaedd2fa3bf1859656fa4c2
Rodolfo Good <rodolfo.good@mrjaffesclass.com>
2023-05-16 11:54:25 -0700

    I'm going to cry if tihs doesn't push waaaaaaa :)

--- src/game/Constants.java
+++ src/game/Constants.java
@@ -8,6 +8,9 @@ public class Constants {
 
 	public static final int MINI_BOARD_AMOUNT = 4;
 
+	public static final int WINDOW_HEIGHT = 800;
+	public static final int WINDOW_WIDTH = 800;
+
 	// Player Colors
 	public static final int EMPTY = 0;
 	public static final int BLACK = -1;
