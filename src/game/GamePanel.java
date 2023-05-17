cea262f851a2c41c2dfae2976c23bf30d6ed19e0
RudyG252 <rudyagood@gmail.com>
2023-05-17 02:16:15 -0700

    ummmmmmm
    a work in progress

--- src/game/GamePanel.java
+++ src/game/GamePanel.java
@@ -219,16 +219,16 @@ public class GamePanel extends JFrame implements MessageHandler, MouseListener {
 
     private void setCell(int row, int col, int color) {
         switch (color) {
-            case 1 -> {
+            case Constants.WHITE -> {
                 cells[row][col].setIcon(new ImageIcon("src/game/images/green.png"));
                 cells[row][col].setValue(color);
             }
-            case -1 -> {
-                cells[row][col].setIcon(new ImageIcon("src/game/images/blue.png"));
+            case Constants.BLACK -> {
+                cells[row][col].setIcon(new ImageIcon("src/game/images/blackMarble.png"));
                 cells[row][col].setValue(color);
             }
             default -> {
-                cells[row][col].setIcon(null);
+                cells[row][col].setIcon(new ImageIcon("src/game/images/blankSquare.png"));
                 cells[row][col].setValue(color);
             }
         }
